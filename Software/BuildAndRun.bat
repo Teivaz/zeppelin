a928e318706a2541ec3658b37f418bbbe10a9fdd
Teivaz <o.kuznietsov@gmailc.om>
2013-08-11 12:39:45 +0300

    Removed old project

--- Software/BuildAndRun.bat
+++
@@ -1,7 +0,0 @@
-@ECHO OFF
-@ECHO =========== Building project ===========
-call Target.bat
-@ECHO OFF
-@ECHO =========== Writting to MCU ===========
-call WriteFlash.bat
-@ECHO =========== Finished ===========
