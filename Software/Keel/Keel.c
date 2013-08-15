c139e2fb4c4c69fc063a4bdb46cfd7a649c3fcfc
Teivaz <o.kuznietsov@gmailc.om>
2013-08-16 01:17:34 +0300

    Added new project for "keel" part of the zeppelin Added lib for soft SPI

---
+++ Software/Keel/Keel.c
@@ -0,0 +1,17 @@
+/*
+ * Keel.c
+ *
+ * Created: 16.08.2013 1:14:42
+ *  Author: Teivaz
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
