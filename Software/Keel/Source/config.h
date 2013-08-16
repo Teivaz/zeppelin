4b195981f4e66b0ae493095b2993cb874525be5b
Teivaz <o.kuznietsov@gmailc.om>
2013-08-16 11:22:13 +0300

    Added config and utils for Keel

---
+++ Software/Keel/Source/config.h
@@ -0,0 +1,11 @@
+#ifndef _CONFIG_H_
+#define _CONFIG_H_
+
+#define AXON_MOSI		PB1
+#define AXON_CLK		PB2
+#define DENDRITE_CLK	PB2
+#define DENDRITE_MOSI	PB3
+#define DENDRITE_MISO	PB4
+#define DENDRITE_IRQ	PB5
+
+#endif //_CONFIG_H_
---
+++ Software/Keel/Source/types.h
@@ -0,0 +1,24 @@
+#ifndef TYPES_H_
+#define TYPES_H_
+
+#ifndef __cplusplus
+typedef enum {false = 0, true = !false} bool;
+#endif
+
+
+#define SET_BIT(REG, BIT)     ((REG) |= (1<<BIT))
+
+#define CLEAR_BIT(REG, BIT)   ((REG) &= ~(1<<BIT))
+
+#define READ_BIT(REG, BIT)    ((REG) & (1<<BIT))
+
+#define CLEAR_REG(REG)        ((REG) = (0x0))
+
+#define WRITE_REG(REG, VAL)   ((REG) = (VAL))
+
+#define READ_REG(REG)         ((REG))
+
+#define MODIFY_REG(REG, CLEARMASK, SETMASK)  WRITE_REG((REG), (((READ_REG(REG)) & (~(CLEARMASK))) | (SETMASK)))
+
+
+#endif /* TYPES_H_ */
