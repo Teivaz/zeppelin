829b35ea22417f861d3d39bebd92b200b9735d86
Teivaz <o.kuznietsov@gmail.com>
2013-08-25 12:25:17 +0300

    Split types.h to types and utils

--- Software/Libs/types.h
+++ Software/Libs/types.h
@@ -1,35 +1,8 @@
-#ifndef TYPES_H_
-#define TYPES_H_
+#ifndef _TYPES_H_
+#define _TYPES_H_
 
 #ifndef __cplusplus
 typedef enum {false = 0, true = !false} bool;
 #endif
 
-
-#define SET_BIT(REG, BIT)     ((REG) |= (1<<BIT))
-
-#define CLEAR_BIT(REG, BIT)   ((REG) &= ~(1<<BIT))
-
-#define READ_BIT(REG, BIT)    ((REG) & (1<<BIT))
-
-#define CLEAR_REG(REG)        ((REG) = (0x0))
-
-#define WRITE_REG(REG, VAL)   ((REG) = (VAL))
-
-#define READ_REG(REG)         ((REG))
-
-#define MODIFY_REG(REG, CLEARMASK, SETMASK)  WRITE_REG((REG), (((READ_REG(REG)) & (~(CLEARMASK))) | (SETMASK)))
-
-
-
-inline char CRC(char* data, uint8_t size)
-{
-	char ret = 0;
-	for(uint8_t i = 0; i < size; ++i)
-	{
-		ret += data[i];
-	}
-	return ret;
-}
-
-#endif /* TYPES_H_ */
+#endif /* _TYPES_H_ */
