ac1729dbd8931337c1df98ba8abb905576df38fb
teivaz <o.kuznietsov@gmail.com>
2016-06-19 01:12:11 +0200

    Update Keel config for new MCU type (attiny2313)

--- Software/Keel/Source/config.h
+++ Software/Keel/Source/config.h
@@ -1,13 +1,26 @@
 #ifndef CONFIG_H_
 #define CONFIG_H_
 
-#define CLK				PB2
-#define DBG				PB3
-#define INT				PB3
-#define DENDRITE_CSN	PB4
-#define DENDRITE_MOSI	PB3
-#define DENDRITE_MISO	PB4
-#define DENDRITE_IRQ	PB5
+#define DEVICE_ATTINY2313
+
+#ifdef DEVICE_ATTINY13
+	#define CLK				PB2
+	#define DBG				PB3
+	#define INT				PB3
+	#define DENDRITE_CSN	PB4
+	#define DENDRITE_MOSI	PB3
+	#define DENDRITE_MISO	PB4
+	#define DENDRITE_IRQ	PB5
+	
+#elif defined DEVICE_ATTINY2313
+	#define DENDRITE_SCK	PB7
+	#define DENDRITE_CSN	PB3
+	#define DENDRITE_MOSI	PB5
+	#define DENDRITE_MISO	PB6
+	#define DENDRITE_IRQ	PB4
+	#define DENDRITE_CE		PB3
+	
+#endif
 
 #define STREAM_BUFFER_SIZE  32
 
